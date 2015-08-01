48295e47a7ec2d736d53fe773bfdba30c76c6d8f
Genesis Guerrero <genesisdaft@gmail.com>
2015-08-01 10:40:39 -0500

    test de oauth2 toolkit

--- and_music_for_all/urls.py
+++ and_music_for_all/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'and_music_for_all.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('api.urls')),
+    url(r'', include('website.urls')),
 )
